93724b2fae6e9540d5b9b733c56df30e8037870e
Mahashwe <maha.tomholland@gmail.com>
2026-02-17 10:29:19 +0530

    Refactor code structure for improved readability and maintainability

--- myApp/app/(tabs)/index.tsx
+++ myApp/app/(tabs)/index.tsx
@@ -1,18 +1,18 @@
-import { Image } from 'expo-image';
 import { Platform, StyleSheet, View } from 'react-native';
-
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
+const logo = require('../../assets/images/1.jpg');
 
 
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "plum" }}>
       <Text style={{ fontSize: 50, color: "black", fontStyle: "italic" }}>Hello World</Text>
+      <Image source={logo} style={{ width: 200, height: 200 }} />
     </View>
   );
 }
