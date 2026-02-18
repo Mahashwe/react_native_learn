6df0d3d4d1c8e4464cf4a89f84b71cb90a4208dc
Mahashwe <maha.tomholland@gmail.com>
2026-02-18 17:45:11 +0530

    feat: initialize input project with package.json, reset script, and TypeScript configuration
    - Added package.json with dependencies and scripts for the input project.
    - Created reset-project.js script to reset the project structure and manage existing files.
    - Added tsconfig.json for TypeScript configuration extending from expo's base.

---
+++ input/app/(tabs)/index.tsx
@@ -0,0 +1,54 @@
+import { Image } from "expo-image";
+import {
+  Platform,
+  SafeAreaViewBase,
+  StyleSheet,
+  TextInput,
+  View,
+} from "react-native";
+
+import { HelloWave } from "@/components/hello-wave";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          autoCapitalize="none"
+        />
+      </SafeAreaView>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: "#9cbbf9",
+  },
+  form: {
+    gap: 15,
+    color: "#fffefe",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#fffefe",
+    padding: 12,
+    borderRadius: 8,
+    color: "#fffefe",
+  },
+});
