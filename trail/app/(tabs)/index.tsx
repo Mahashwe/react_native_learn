202112fa93c6dc7bedda1ba615f6f7391c461ea8
Mahashwe <maha.tomholland@gmail.com>
2026-02-18 16:18:20 +0530

    feat: refactor HomeScreen to use FlatList for improved performance and layout

--- trail/app/(tabs)/index.tsx
+++ trail/app/(tabs)/index.tsx
@@ -1,5 +1,12 @@
 import { Image } from "expo-image";
-import { Platform, StyleSheet, View, Text, StatusBar } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  FlatList,
+} from "react-native";
 
 import { HelloWave } from "@/components/hello-wave";
 import ParallaxScrollView from "@/components/parallax-scroll-view";
@@ -8,19 +15,24 @@ import { ThemedView } from "@/components/themed-view";
 import { Link } from "expo-router";
 import PokemonList from "@/data.json";
 import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <View style={styles.titleContainer}>
-      {PokemonList.map((pokemon) => (
-        <View style={styles.card} key={pokemon.id}>
-          <ScrollView style={styles.scrollviewkey}>
-            <Text style={styles.Cardtext}>
-              {pokemon.name} - {pokemon.type}
-            </Text>
-          </ScrollView>
-        </View>
-      ))}
+      <SafeAreaView>
+        <FlatList
+          data={PokemonList}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.Cardtext}>{item.name}</Text>
+              <Text style={styles.Cardtext}>{item.type}</Text>
+            </View>
+          )}
+          contentContainerStyle={styles.scrollviewkey}
+        />
+      </SafeAreaView>
     </View>
   );
 }
