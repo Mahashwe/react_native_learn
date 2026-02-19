97d98b4f3c5143009258088ab8818ed12bf0d402
Mahashwe <maha.tomholland@gmail.com>
2026-02-19 13:23:24 +0530

    feat: initialize network project with package.json, reset script, and TypeScript configuration
    - Added package.json with dependencies and scripts for the network project.
    - Created reset-project.js script to reset the project structure and manage existing files.
    - Introduced tsconfig.json for TypeScript configuration with strict settings and path mappings.

---
+++ network/app/(tabs)/index.tsx
@@ -0,0 +1,96 @@
+import { Image } from "expo-image";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+
+import { HelloWave } from "@/components/hello-wave";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { useEffect } from "react";
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+};
+
+export default function HomeScreen() {
+  const [data, setData] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+  const fetchData = async (limit = 10) => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`,
+      );
+      const data = await response.json();
+      setData(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#f27070" />
+      </View>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.ListContainer}>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={{ fontWeight: "bold" }}>{item.title}</Text>
+              <Text>{item.body}</Text>
+            </View>
+          )}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+
+  ListContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  card: {
+    marginBottom: 16,
+    borderBlockColor: "#ccc",
+    borderWidth: 5,
+    borderRadius: 8,
+    padding: 16,
+    backgroundColor: "white",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
