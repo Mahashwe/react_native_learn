bae24199eada54dce819b05782c1139540868aa4
Mahashwe <maha.tomholland@gmail.com>
2026-02-20 17:34:23 +0530

    feat: replace Drawer navigation with Tabs and update dependencies

--- DrawerExpoApp/app/_layout.tsx
+++ DrawerExpoApp/app/_layout.tsx
@@ -1,11 +1,12 @@
-import { Drawer } from "expo-router/drawer";
+import { Tabs } from "expo-router";
 import "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <Drawer>
-      <Drawer.Screen name="dashboard" options={{ title: "Dashboard" }} />
-      <Drawer.Screen name="settings" options={{ title: "Settings" }} />
-    </Drawer>
+    <Tabs>
+      <Tabs.Screen name="dashboard" options={{ title: "Dashboard" }} />
+      <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+      <Tabs.Screen name="app" options={{ href: null }} />
+    </Tabs>
   );
 }
